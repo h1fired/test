2a6ced11d82c92806de1b4fa24d64c244b56e002
h1fired <misha01213@gmail.com>
2023-05-19 16:29:29 +0300

    add annotation to division func

--- utils/math_func.py
+++ utils/math_func.py
@@ -1,6 +1,8 @@
 def division(a, b):
+    """ Division two numbers """
     if type(a) not in [float, int] or type(b) not in [float, int]:
         raise TypeError
     if b == 0: 
         raise ZeroDivisionError
+    
     return a / b
